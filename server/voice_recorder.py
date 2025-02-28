6a43e8df0d282767d03e1fe77033562d11d3edd0
RomeoCoding <165141163+RomeoCoding@users.noreply.github.com>
2025-02-28 15:46:31 +0200

    update

--- server/voice_recorder.py
+++ server/voice_recorder.py
@@ -5,7 +5,7 @@ import torchaudio
 duration = 5
 samplerate = 16000
 print("Recording... Speak now!")
-audio = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=None, dtype='float32')
+audio = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=2, dtype='float32')
 sd.wait()
 torchaudio.save("my_voice.wav", torch.tensor(audio), samplerate)
 print("Voice sample saved as my_voice.wav")
